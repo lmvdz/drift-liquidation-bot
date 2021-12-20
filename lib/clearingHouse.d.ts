6258beefd2506e4b31bea2d42242d52b9539a67c
unknown <lmvanderzande@gmail.com>
2021-12-20 10:39:22 -0600

    latest worker stuff

--- lib/clearingHouse.d.ts
+++
@@ -1,7 +0,0 @@
-import { ClearingHouse } from '@drift-labs/sdk';
-import pkg from '@project-serum/anchor';
-declare const _default: {
-    clearingHouse: ClearingHouse;
-    mainnetConnection: pkg.web3.Connection;
-};
-export default _default;
