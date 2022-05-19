be1b0071f65ae14e8d565ed7ae85a717409d031c
lmvdz <lmvanderzande@gmail.com>
2022-05-18 20:33:09 -0500

    latest

--- src/multi.ts
+++ src/multi.ts
@@ -5,9 +5,6 @@ import { fork, ChildProcess } from 'child_process';
 const workers : Map<string, ChildProcess> = new Map<string, ChildProcess>();
 
 
-
-
-
 function createUUID() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
